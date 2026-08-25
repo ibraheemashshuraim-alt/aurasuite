9aa0efe6c44be42c9a8691667582794a7ccd4079
Abdullah <abdullah@example.com>
2026-08-25 20:01:34 +0500

    Add debug info to badge

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -2913,8 +2913,14 @@ export default function AppContainer() {
   // ══════════════════ MAIN APP ══════════════════
   return (
     <main className="relative min-h-screen">
-        <div className="fixed bottom-4 right-4 z-[9999999999] bg-green-500 text-white font-bold px-4 py-2 rounded-full shadow-lg">
-          Deployed: Aug 25 19:55
+        <div className="fixed bottom-4 right-4 z-[9999999999] bg-green-500 text-white text-xs font-bold px-4 py-2 rounded-xl shadow-lg whitespace-pre-wrap">
+          {`Deployed: Aug 25 20:01
+User Locked: ${currentUser?.is_locked}
+User Force Unlock: ${currentUser?.force_unlocked}
+Org 24/7: ${activeOrg?.working_hours?.is_24_7}
+Org Start: ${activeOrg?.working_hours?.start}
+Org End: ${activeOrg?.working_hours?.end}
+Current Time: ${new Date().getHours()}:${new Date().getMinutes()}`}
         </div>
       <div className="flex min-h-screen bg-luxury-bg text-[#f3f1f5] relative">
 
